b0af022a738beb4c2fcd0c69f9c0f0be4f655a14
Gabriel Lopez <gabo_71096@hotmail.com>
2025-10-01 11:05:47 -0400

    feat: add relevant associations to client and provider

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,4 @@
 class Client < ApplicationRecord
+  has_many :appointments, dependent: :destroy
+  has_many :providers, through: :appointments
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,2 +1,5 @@
 class Provider < ApplicationRecord
+  has_many :appointments, dependent: :destroy
+  has_many :availability_windows, dependent: :destroy
+  has_many :clients, through: :appointments
 end
