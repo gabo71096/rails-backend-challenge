f2dacdfa9fd211b1d4d35cdbd2160181cd9b4ad2
Gabriel Lopez <gabo_71096@hotmail.com>
2025-10-01 11:09:30 -0400

    feat: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,21 @@
 # Seed data is intentionally left blank.
 # Candidates can add sample clients/providers/availabilities once their schema is in place.
+
+if Rails.env.development?
+  Client.create(
+    name: "John",
+    last_name: "Doe"
+  )
+  Provider.create(
+    name: "Jane 1",
+    last_name: "Doe"
+  )
+  Provider.create(
+    name: "Jane 2",
+    last_name: "Doe"
+  )
+  Provider.create(
+    name: "Jane 3",
+    last_name: "Doe"
+  )
+end
